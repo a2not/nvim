74a366b7deb5fbcb2b0735bc5c140da3ff16c0f5
a2not <fb503d52.af1c4d@gmail.com>
2024-04-09 17:07:59 +0900

    restore format on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -14,10 +14,10 @@ return {
   },
   opts = {
     notify_on_error = false,
-    -- format_on_save = {
-    --   timeout_ms = 500,
-    --   lsp_fallback = true,
-    -- },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
     formatters_by_ft = {
       lua = { 'stylua' },
       go = { 'gofmt', 'goimports' },
