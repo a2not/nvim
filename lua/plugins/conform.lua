d5fa2a07d878ce51aa5850334552f784474839f9
a2not <fb503d52.af1c4d@gmail.com>
2024-04-10 09:32:59 +0900

    disable format-on-save on js/ts

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -14,10 +14,23 @@ return {
   },
   opts = {
     notify_on_error = false,
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
+    format_on_save = function(bufnr)
+      -- Disable autoformat on certain filetypes
+      local ignore_filetypes = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' }
+      if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+        return
+      end
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      -- Disable autoformat for files in a certain path
+      local bufname = vim.api.nvim_buf_get_name(bufnr)
+      if bufname:match('/node_modules/') then
+        return
+      end
+      return { timeout_ms = 500, lsp_fallback = true }
+    end,
     formatters_by_ft = {
       lua = { 'stylua' },
       go = { 'gofmt', 'goimports' },
