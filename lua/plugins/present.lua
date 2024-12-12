eddba4fe6b2e7444484f899aecce840ea4fb77f4
a2not <a2not.dev@gmail.com>
2024-12-12 22:11:06 +0900

    define present separator

--- lua/plugins/present.lua
+++ lua/plugins/present.lua
@@ -4,6 +4,10 @@ return {
     options = {
       width = 60,
     },
+    separator = {
+      markdown = '^<!%-%- presenting%.nvim sep %-%->',
+    },
+    keep_separator = false,
   },
   cmd = { 'Presenting' },
 }
