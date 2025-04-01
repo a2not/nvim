c8e9c2f4e7cbbe709cbd7c15d55c9f96e73560d4
a2not <a2not.dev@gmail.com>
2025-04-01 11:14:22 +0900

    fix for golangci-lint v2

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -90,7 +90,19 @@ return {
       },
       gopls = {},
       goimports = {},
-      golangci_lint_ls = {},
+      golangci_lint_ls = {
+        -- NOTE: while waiting for https://github.com/nametake/golangci-lint-langserver/issues/51
+        init_options = {
+          command = {
+            'golangci-lint',
+            'run',
+            '--output.json.path',
+            'stdout',
+            '--show-stats=false',
+            '--issues-exit-code=1',
+          },
+        },
+      },
       html = {},
       templ = {},
       rust_analyzer = {},
