d16cb391a0075324a8d679af1c43c67bd62ca0ac
a2not <a2not.dev@gmail.com>
2025-05-12 10:06:40 +0900

    use builtin comment feature

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -1,20 +1,5 @@
 return {
   {
-    'numToStr/Comment.nvim',
-    event = 'VeryLazy',
-    dependencies = {
-      {
-        'JoosepAlviste/nvim-ts-context-commentstring',
-        lazy = true,
-        opts = {
-          enable_autocmd = false,
-        },
-      },
-    },
-    config = true,
-  },
-
-  {
     'norcalli/nvim-colorizer.lua',
     opts = {
       '*',
