6264fd3d9a5079ed784f87d4f49873e81f88fbc9
a2not <a2not.dev@gmail.com>
2025-06-17 17:48:38 +0900

    noiceでmacro recording
    Signed-off-by: a2not <a2not.dev@gmail.com>

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -11,7 +11,17 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = true,
+    opts = {
+      sections = {
+        lualine_x = {
+          {
+            require('noice').api.status.mode.get,
+            cond = require('noice').api.status.mode.has,
+            color = { fg = '#ff9e64' },
+          },
+        },
+      },
+    },
   },
 
   {
