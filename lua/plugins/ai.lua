5321621a8728e8f0a4ac5db192c760778b590bb7
a2not <a2not.dev@gmail.com>
2025-07-31 10:52:40 +0900

    commit acceptable
    ref: https://github.com/CopilotC-Nvim/CopilotChat.nvim/issues/1124#issuecomment-3137865656
    
    Signed-off-by: a2not <a2not.dev@gmail.com>

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -21,9 +21,20 @@ return {
       { 'zbirenbaum/copilot.lua' },
       { 'nvim-lua/plenary.nvim', branch = 'master' },
     },
-    opts = {
-      chat_autocomplete = false,
-    },
+    config = function()
+      local chat = require('CopilotChat')
+      local select = require('CopilotChat.select')
+      chat.setup({
+        chat_autocomplete = false,
+        prompts = {
+          Commit = {
+            prompt = 'Write commit message for the change with commitizen convention. Keep the title under 50 characters and wrap message at 72 characters. Format as a gitcommit code block.',
+            sticky = '#gitdiff:staged',
+            selection = select.buffer,
+          },
+        },
+      })
+    end,
   },
   {
     'yetone/avante.nvim',
